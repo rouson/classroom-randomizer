2f7ccf5cfe56440da36be94aa936f40eaff2810a
Damian Rouson <damian@sourceryinstitute.org>
2019-07-28 16:58:10 -0700

    Morph Rooms list into Courses list

--- web/main.js
+++ web/main.js
@@ -1,62 +1,43 @@
 const BASE_SITE = "http://localhost:8888";
-const ROOM_NUMBER_REGEX = /([a-z]+)(\d+)/i;
+const YEAR_REGEX = /([a-z]+) (\d+)/i;
 
-const createRoomElement = roomSpec => {
-  const roomId = roomSpec.ID;
-  const numberOfSeats = roomSpec.number_of_seats;
-  const match = ROOM_NUMBER_REGEX.exec(roomId);
+const createCourseElement = courseSpec => {
+  const courseTerm = courseSpec.term;
+  const courseSubject = courseSpec.subject;
+  const match = YEAR_REGEX.exec(courseTerm);
 
   if (match === null) {
-    throw new Error("Invalid Room Id!");
+    throw new Error("Invalid Course Term!");
   }
 
-  const roomNumber = match[2];
+  const year = match[2];
 
-  const $roomDiv = $("<div>");
+  const $courseDiv = $("<div>");
 
-  $roomDiv.append(`<h2> ${roomNumber} </h2>`);
-  $roomDiv.append(`ID: ${roomId}<br>`);
-  $roomDiv.append(`number of seats: ${numberOfSeats}<br>`);
+  $courseDiv.append(`<h2> ${year} </h2>`);
+  $courseDiv.append(`Term: ${courseTerm}<br>`);
+  $courseDiv.append(`Subject: ${courseSubject}<br>`);
 
-  $("#classrooms").append($roomDiv);
+  $("#courses").append($courseDiv);
 };
 
-const fetchRoomData = roomFilename => {
-  const url = `${BASE_SITE}/${roomFilename}`;
+const fetchCourseData = courseFilename => {
+  const url = `${BASE_SITE}/${courseFilename}`;
 
   return window
     .fetch(url)
     .then(response => response.json())
     .catch(error =>
-      alert(`There was an error reading from the file: ${roomFilename}`)
+      alert(`There was an error reading from the file: ${courseFilename}`)
     );
 };
 
-const displayClassroomInfo = () => {
-  fetchRoomData("web/data/rooms.json").then(roomList => {
-    roomList.forEach(createRoomElement);
+const displayCourseInfo = () => {
+  fetchCourseData("web/data/courses.json").then(courseList => {
+    courseList.forEach(createCourseElement);
   });
 };
 
 $(() => {
-  displayClassroomInfo();
+  displayCourseInfo();
 });
-
-// // Damian to do  vvv
-
-// // var classes_json =
-// // '{ "course" : [ {"subject":"Math 7", "trimester":"Spring 2019", "section":"1st period",  "students":["Zendo", "Jeremiah"]}, {"subject":"Spanish 7", "trimester":"Spring 2019", "section":"2nd period", "students":["Hamilton", "Avi"]} ]}';
-
-// // class_list = JSON.parse(classes_json)
-
-// // document.write("<h3>Class 0</h2>")
-// // document.write("Subject: ",class_list.course[0].subject,"<br>");
-// // document.write("Trimester: ",class_list.course[0].trimester,"<br>");
-// // document.write("Section: ",class_list.course[0].section,"<br>");
-// // document.write("Students: ",class_list.course[0].students,"<br>");
-
-// // document.write("<h3>Class 1</h2>")
-// // document.write("Subject: ",class_list.course[1].subject,"<br>");
-// // document.write("Trimester: ",class_list.course[1].trimester,"<br>");
-// // document.write("Section: ",class_list.course[1].section,"<br>");
-// // document.write("Students: ",class_list.course[1].students,"<br>");
